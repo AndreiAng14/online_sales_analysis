789e84e4da1035355569d410a20b632ce5f38654
Andrei <andrei@gmail.com>
2025-07-08 22:09:37 +0300

    From main.py are removed: the functions for showing products and total quantity. The products quantities are changed.

--- main.py
+++ main.py
@@ -4,31 +4,21 @@ from product import Product
 product_manager=ProductManager()
 
 products=[
-    Product('Laptop Gaming Asus',2_3455.56,100),
+    Product('Laptop Gaming Asus',2_3455.56,12),
     Product('Televizor Samsung',1_1234,10),
-    Product('Cuptor Microunde Beko',455.01,54),
-    Product('Frigider Artic',6_3455.99,40),
-    Product('Tastatura Logitech',34,166),
+    Product('Cuptor Microunde Beko',455.01,5),
+    Product('Frigider Artic',6_3455.99,44),
+    Product('Tastatura Logitech',34,16),
 ]
 
 def add_product(products):
     for p in products:
         product_manager.add_product(p)
 
-def show_products():
-    print('')
-    product_manager.show_all()
-
-def show_total():
-    print('')
-    product_manager.average()
-
 def remove_product(name):
     print('')
     product_manager.remove_by_name(name)
 
 add_product(products)
-# show_products()
-# show_total()
 remove_product()
 
